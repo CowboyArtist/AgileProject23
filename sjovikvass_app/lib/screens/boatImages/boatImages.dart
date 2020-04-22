ef53df9d6b9c99357f61dba87c0d13de3efb5efd
Jesper <jesper@jepsoncompedit.se>
2020-04-22 09:39:20 +0200

    Added dialog for launching camera or gallery according to design best practice

--- sjovikvass_app/lib/screens/boatImages/boatImages.dart
+++ sjovikvass_app/lib/screens/boatImages/boatImages.dart
@@ -1,3 +1,7 @@
+import 'dart:io';
+
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:sjovikvass_app/styles/my_colors.dart';
 import 'package:sjovikvass_app/styles/commonWidgets/detailAppBar.dart';
@@ -10,11 +14,67 @@ class BoatImages extends StatefulWidget {
 
 class _BoatImagesState extends State<BoatImages> {
 
+  _showSelectImageDialog() {
+    return Platform.isIOS ? _iosBottomSheet() : _androidDialog();
+  }
+
+  _iosBottomSheet() {
+    showCupertinoModalPopup(
+        context: context,
+        builder: (BuildContext context) {
+          return CupertinoActionSheet(
+            title: Text('Lägg till foto'),
+            actions: <Widget>[
+              CupertinoActionSheetAction(
+                child: Text('Ta ett foto'),
+                onPressed: () => print('Kör metod för att ta foto'),
+              ),
+              CupertinoActionSheetAction(
+                child: Text('Från galleriet'),
+                onPressed: () => print('Kör metod för att välja från kamerarullen'),
+              ),
+            ],
+            cancelButton: CupertinoActionSheetAction(
+              child: Text('Avbryt'),
+              onPressed: () => Navigator.pop(context),
+            ),
+          );
+        });
+  }
+
+  _androidDialog() {
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return SimpleDialog(
+            title: Text('Lägg till foto'),
+            children: <Widget>[
+              SimpleDialogOption(
+                child: Text('Ta ett foto'),
+                onPressed: () => print('Kör metod för att ta foto'),
+              ),
+              SimpleDialogOption(
+                child: Text('Från kamerarullen'),
+                onPressed: () => print('Kör metod för att välja från kamerarullen'),
+              ),
+              SimpleDialogOption(
+                child: Text(
+                  'Avbryt',
+                  style: TextStyle(color: Colors.redAccent),
+                ),
+                onPressed: () => Navigator.pop(context),
+              )
+            ],
+          );
+        });
+  }
+
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: DetailAppBar.buildAppBar('Bilder'),
+      body: Center(child: FlatButton(child: Text('Testa dialogruta'), onPressed: _showSelectImageDialog,)),
     );
   }
 }
