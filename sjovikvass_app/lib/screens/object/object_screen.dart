4bbdf61675a3c6212aaa454999592fc9402bc206
David <david.lidevi@gmail.com>
2020-04-24 14:40:53 +0200

    Added two navigation buttons to the detail screen

--- sjovikvass_app/lib/screens/object/object_screen.dart
+++ sjovikvass_app/lib/screens/object/object_screen.dart
@@ -18,33 +18,65 @@ class _ObjectScreenState extends State<ObjectScreen> {
     return Scaffold(
       appBar: AppBar(title: Text('Detaljvy')),
       body: Center(
-        child: GestureDetector(
-          onTap: () => Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (context) => AddObjectScreen(),
-              )),
-          child: Container(
-            margin: EdgeInsets.fromLTRB(40.0, 0.0, 40.0, 40.0),
-            padding: EdgeInsets.symmetric(vertical: 16.0),
-            height: 50.0,
-            width: 50.0,
-            decoration: BoxDecoration(
-                color: MyColors.lightBlue,
-                boxShadow: [
-                  BoxShadow(
-                      color: Colors.black12,
-                      offset: Offset(3, 3),
-                      blurRadius: 5.0)
-                ],
-                borderRadius: BorderRadius.circular(10.0)),
-            child: Center(
-                child: Icon(
-              Icons.photo_camera,
-              size: 32.0,
-              color: MyColors.primary,
-            )),
-          ),
+        child: new Row(
+          children: <Widget>[
+            GestureDetector(
+              onTap: () => Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => AddObjectScreen(),
+                  )),
+              child: Container(
+                margin: EdgeInsets.fromLTRB(40.0, 0.0, 40.0, 40.0),
+                padding: EdgeInsets.symmetric(vertical: 16.0),
+                height: 50.0,
+                width: 50.0,
+                decoration: BoxDecoration(
+                    color: MyColors.lightBlue,
+                    boxShadow: [
+                      BoxShadow(
+                          color: Colors.black12,
+                          offset: Offset(3, 3),
+                          blurRadius: 5.0)
+                    ],
+                    borderRadius: BorderRadius.circular(10.0)),
+                child: Center(
+                    child: Icon(
+                  Icons.photo_camera,
+                  size: 32.0,
+                  color: MyColors.primary,
+                )),
+              ),
+            ),
+            GestureDetector(
+              onTap: () => Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => AddObjectScreen(),
+                  )),
+              child: Container(
+                margin: EdgeInsets.fromLTRB(40.0, 0.0, 40.0, 40.0),
+                padding: EdgeInsets.symmetric(vertical: 16.0),
+                height: 50.0,
+                width: 50.0,
+                decoration: BoxDecoration(
+                    color: MyColors.lightBlue,
+                    boxShadow: [
+                      BoxShadow(
+                          color: Colors.black12,
+                          offset: Offset(3, 3),
+                          blurRadius: 5.0)
+                    ],
+                    borderRadius: BorderRadius.circular(10.0)),
+                child: Center(
+                    child: Icon(
+                  Icons.build,
+                  size: 32.0,
+                  color: MyColors.primary,
+                )),
+              ),
+            ),
+          ],
         ),
       ),
     );
