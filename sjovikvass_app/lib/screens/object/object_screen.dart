4ccf7beb4c9b800f42de5df4e7ad0c466c158e7d
David <david.lidevi@gmail.com>
2020-04-24 13:43:14 +0200

    Added navigation to boat detail screen

--- sjovikvass_app/lib/screens/object/object_screen.dart
+++ sjovikvass_app/lib/screens/object/object_screen.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:sjovikvass_app/models/stored_object_model.dart';
-
+import 'package:sjovikvass_app/screens/addObject/add_object_screen.dart';
+import 'package:sjovikvass_app/styles/my_colors.dart';
 
 //The screen that shows the overview of one specific object.
 class ObjectScreen extends StatefulWidget {
-  
   final StoredObject object;
   ObjectScreen({this.object});
 
@@ -17,7 +17,36 @@ class _ObjectScreenState extends State<ObjectScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Detaljvy')),
-     body: Center(child: Text( 'Vy för: ${widget.object.title}')),
+      body: Center(
+        child: GestureDetector(
+          onTap: () => Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => AddObjectScreen(),
+              )),
+          child: Container(
+            margin: EdgeInsets.fromLTRB(40.0, 0.0, 40.0, 40.0),
+            padding: EdgeInsets.symmetric(vertical: 16.0),
+            height: 50.0,
+            width: 50.0,
+            decoration: BoxDecoration(
+                color: MyColors.lightBlue,
+                boxShadow: [
+                  BoxShadow(
+                      color: Colors.black12,
+                      offset: Offset(3, 3),
+                      blurRadius: 5.0)
+                ],
+                borderRadius: BorderRadius.circular(10.0)),
+            child: Center(
+                child: Icon(
+              Icons.photo_camera,
+              size: 32.0,
+              color: MyColors.primary,
+            )),
+          ),
+        ),
+      ),
     );
   }
 }
