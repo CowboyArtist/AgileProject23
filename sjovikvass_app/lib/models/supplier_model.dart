532a42b8e4ae3e55f3c7982709c58633d2275108
David <david.lidevi@gmail.com>
2020-04-28 08:18:10 +0200

    Defined how to save a Supplier in the DB

---
+++ sjovikvass_app/lib/models/supplier_model.dart
@@ -0,0 +1,32 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class Supplier {
+  final String id;
+  final String companyName;
+  final String description;
+  final String phoneNr;
+  final String email;
+  final String imageUrl;
+
+  Supplier({
+    this.id,
+    this.companyName,
+    this.description,
+    this.phoneNr,
+    this.email,
+    this.imageUrl,
+  });
+
+  //Method to create a Supplier from database document
+
+  factory Supplier.fromDoc(DocumentSnapshot doc) {
+    return Supplier(
+      id: doc.documentID,
+      companyName: doc['companyName'],
+      description: doc['description'],
+      phoneNr: doc['phoneNr'],
+      email: doc['email'],
+      imageUrl: doc['imageUrl'],
+    );
+  }
+}
