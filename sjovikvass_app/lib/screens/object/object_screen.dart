98c3a8c5e08e19305cef9f16f7de19d09c655a4f
Jesper <jesper@jepsoncompedit.se>
2020-04-28 17:25:05 +0200

    Started on UI of objectScreen

--- sjovikvass_app/lib/screens/object/object_screen.dart
+++ sjovikvass_app/lib/screens/object/object_screen.dart
@@ -4,6 +4,7 @@ import 'package:sjovikvass_app/models/stored_object_model.dart';
 import 'package:sjovikvass_app/screens/boatImages/boatImages.dart';
 import 'package:sjovikvass_app/screens/documents/document_screen.dart';
 import 'package:sjovikvass_app/screens/workPage/work_page.dart';
+import 'package:sjovikvass_app/styles/my_colors.dart';
 
 //The screen that shows the overview of one specific object.
 class ObjectScreen extends StatefulWidget {
@@ -23,16 +24,19 @@ class _ObjectScreenState extends State<ObjectScreen> {
           Stack(
             children: <Widget>[
               Container(
-                height: 240.0,
+                height: 200.0,
                 width: double.infinity,
-                child: widget.object.imageUrl == null ?  Image.asset('assets/images/placeholder_boat.jpg',
-                    fit: BoxFit.cover)
-                    : 
-                    Image(image: CachedNetworkImageProvider(widget.object.imageUrl), fit: BoxFit.cover,)
-                    ,
+                child: widget.object.imageUrl == null
+                    ? Image.asset('assets/images/placeholder_boat.jpg',
+                        fit: BoxFit.cover)
+                    : Image(
+                        image:
+                            CachedNetworkImageProvider(widget.object.imageUrl),
+                        fit: BoxFit.cover,
+                      ),
               ),
               Container(
-                height: 240.0,
+                height: 200.0,
                 width: double.infinity,
                 decoration: BoxDecoration(
                   gradient: LinearGradient(
@@ -41,20 +45,81 @@ class _ObjectScreenState extends State<ObjectScreen> {
                     end: Alignment.bottomCenter,
                   ),
                 ),
-                child: Center(
-                    child: Text(
-                  widget.object.title,
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 28.0),
-                )),
+              ),
+              Positioned(
+                bottom: 16.0,
+                left: 22.0,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      widget.object.title,
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 28.0),
+                    ),
+                    Text(
+                      widget.object.description,
+                      style: TextStyle(color: Colors.white, fontSize: 20.0),
+                    )
+                  ],
+                ),
               ),
               AppBar(
                 backgroundColor: Colors.transparent,
                 elevation: 0.0,
               ),
             ],
+          ),
+          SizedBox(
+            height: 20.0,
+          ),
+          Container(
+            margin: EdgeInsets.all(8.0),
+            height: 170.0,
+            width: MediaQuery.of(context).size.width,
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: Container(
+                    margin: EdgeInsets.symmetric(horizontal: 8.0),
+                    padding: EdgeInsets.all(16.0),
+                    decoration: BoxDecoration(
+                        color: MyColors.lightBlue,
+                        borderRadius: BorderRadius.circular(16.0)),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Icon(Icons.swap_horiz,
+                              size: 44.0, color: MyColors.primary),
+                          Column(
+                            children: <Widget>[
+                              widget.object.inDate != null
+                                  ? Text(
+                                      'In: ${widget.object.inDate.toDate().toLocal().toString()}')
+                                  : Text('Indata ej angett'),
+                              widget.object.outDate != null
+                                  ? Text(
+                                      'Ut: ${widget.object.outDate.toDate().toLocal().toString()}')
+                                  : Text('Utdata ej angett'),
+                            ],
+                          ),
+                          Text(widget.object.storageType, style: TextStyle(fontWeight: FontWeight.bold))
+                        ]),
+                  ),
+                ),
+                Expanded(
+                  child: Container(
+                    margin: EdgeInsets.symmetric(horizontal: 8.0),
+                    decoration: BoxDecoration(
+                        color: MyColors.lightBlue,
+                        borderRadius: BorderRadius.circular(16.0)),
+                  ),
+                ),
+              ],
+            ),
           ),
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
@@ -108,7 +173,6 @@ class _ObjectScreenState extends State<ObjectScreen> {
               ),
             ],
           ),
-          
         ],
       ),
     );
