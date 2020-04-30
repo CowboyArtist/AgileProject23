1c937fd4f5df172734d0c8f90aab73661ca70732
Jesper <jesper@jepsoncompedit.se>
2020-04-30 11:18:29 +0200

    Refactor getting month string to service class

---
+++ sjovikvass_app/lib/services/time_service.dart
@@ -0,0 +1,47 @@
+class TimeService {
+  static getMonthString(int monthNumber){
+
+    
+    switch (monthNumber) {
+      case 1:
+        return 'Januari';
+        break;
+      case 2:
+        return 'Februari';
+        break;
+      case 3:
+        return 'Mars';
+        break;
+      case 4:
+        return 'April';
+        break;
+      case 5:
+        return 'Maj';
+        break;
+      case 6:
+        return 'Juni';
+        break;
+      case 7:
+        return 'Juli';
+        break;
+      case 8:
+        return 'Augusti';
+        break;
+      case 9:
+        return 'September';
+        break;
+      case 10:
+        return 'Oktober';
+        break;
+      case 11:
+        return 'November';
+        break;
+      case 12:
+        return 'December';
+        break;
+
+      default:
+        return 'Månad';
+  }
+  }
+}
